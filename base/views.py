ac87752a893108d2512757ac8db4b60edf77eb93
SakshamTolani <sakshamtolani@gmail.com>
2025-02-01 00:13:13 +0530

    feat: Implement FAQ API with language support

--- base/views.py
+++ base/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import FAQ
+from .serializers import FAQSerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 # Create your views here.
+class FAQViewSet(viewsets.ModelViewSet):
+    queryset = FAQ.objects.all()
+    serializer_class = FAQSerializer
+
+    @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
+    def list(self, request, *args, **kwargs):
+        lang = request.query_params.get('lang', 'en')
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True, context={'lang': lang})
+        return Response(serializer.data)
