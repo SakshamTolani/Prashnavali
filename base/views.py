1c141d2596de8ec5022208f2984ca0bcd9607a50
SakshamTolani <sakshamtolani@gmail.com>
2025-02-01 14:11:44 +0530

    feat: Add fallback to English if translation is unavailable

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import FAQ
 from .serializers import FAQSerializer
 from django.utils.decorators import method_decorator
+from django.conf import settings
 from django.views.decorators.cache import cache_page
 
 # Create your views here.
@@ -10,9 +11,13 @@ class FAQViewSet(viewsets.ModelViewSet):
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
 
-    @method_decorator(cache_page(60 * 15))  # Cache for 15 minutes
+    @method_decorator(cache_page(60 * 15))  # 15 minutes
     def list(self, request, *args, **kwargs):
         lang = request.query_params.get('lang', 'en')
+
+        if lang not in settings.AVAILABLE_LANGUAGES:
+            lang = settings.DEFAULT_LANGUAGE
+
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True, context={'lang': lang})
         return Response(serializer.data)
